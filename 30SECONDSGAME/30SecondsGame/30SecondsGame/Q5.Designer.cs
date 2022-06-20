8343436b80d606068aa050d6d3e4559203a1f11c
Christian Mercedes <212072@vistacollege.nl>
2022-06-20 13:18:06 +0200

    .

--- 30SECONDSGAME/30SecondsGame/30SecondsGame/Q5.Designer.cs
+++ 30SECONDSGAME/30SecondsGame/30SecondsGame/Q5.Designer.cs
@@ -30,7 +30,58 @@ namespace _30SecondsGame
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Q5));
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(554, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Vraag";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(639, 513);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(259, 63);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Antwoord 4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 513);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(259, 63);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Antwoord 3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(639, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 63);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Antwoord 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 63);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Antwoord 1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Q5
             // 
@@ -38,13 +89,25 @@ namespace _30SecondsGame
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1199, 700);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Q5";
             this.Text = "Q5";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
